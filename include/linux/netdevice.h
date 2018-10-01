f42614fd9082f2ceb93d6cfde72266055968e7f5
Magnus Karlsson <magnus.karlsson@intel.com>
2018-10-01 14:51:33 +0200

    UPSTREAM: net: add umem reference in netdev{_rx}_queue
    These references to the umem will be used to store information
    on what kind of AF_XDP umem that is bound to a queue id, if any.
    
    Signed-off-by: Magnus Karlsson <magnus.karlsson@intel.com>
    Signed-off-by: Daniel Borkmann <daniel@iogearbox.net>
    (cherry picked from commit 661b8d1b0e3a745e25f05adef2ebd00d830eeea7)
    Signed-off-by: nostalgiceagle <183935403+nostalgiceagle@users.noreply.github.com>

--- include/linux/netdevice.h
+++ include/linux/netdevice.h
@@ -592,6 +592,9 @@ struct netdev_queue {
 
 	/* Subordinate device that the queue has been assigned to */
 	struct net_device	*sb_dev;
+#ifdef CONFIG_XDP_SOCKETS
+	struct xdp_umem         *umem;
+#endif
 /*
  * write-mostly part
  */
@@ -726,6 +729,9 @@ struct netdev_rx_queue {
 	struct kobject			kobj;
 	struct net_device		*dev;
 	struct xdp_rxq_info		xdp_rxq;
+#ifdef CONFIG_XDP_SOCKETS
+	struct xdp_umem                 *umem;
+#endif
 
 	ANDROID_KABI_RESERVE(1);
 	ANDROID_KABI_RESERVE(2);
